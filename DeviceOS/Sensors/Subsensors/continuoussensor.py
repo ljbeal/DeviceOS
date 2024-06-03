2073f555b913b47934fc4ec7487c527d0d0b6716
ljbeal <louis.j.beal@gmail.com>
2024-06-03 23:54:34 +0200

    add continuous sensor skeleton

---
+++ DeviceOS/Sensors/Subsensors/continuoussensor.py
@@ -0,0 +1,12 @@
+from DeviceOS.Sensors.Subsensors.basesubsensor import BaseSubSensor
+
+
+class ContinuousSensor(BaseSubSensor):
+    """
+    Class defining a sensor that runs "continuously"
+
+    The read() method here simply collects the most recent data    
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
