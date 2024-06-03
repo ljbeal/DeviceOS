8a2585d9401b5aca99dcd117ab05a5853abbeb41
ljbeal <louis.j.beal@gmail.com>
2024-06-04 00:11:37 +0200

    basesubsensor -> output

--- DeviceOS/Sensors/Subsensors/output.py
+++ DeviceOS/Sensors/Subsensors/output.py
@@ -1,4 +1,4 @@
-class BaseSubSensor:
+class Output:
     """
     Baseclass to mark the instance of one sensor reading
 
@@ -8,14 +8,19 @@ class BaseSubSensor:
         name: subsensor name
         icon: subsensor icon
         unit: subsensor unit
+        diagnostic: flag this sensor as "diagnostic"
     """
 
-    __slots__ = ["_name", "_icon", "_unit"]
+    __slots__ = ["_name", "_icon", "_unit", "_is_diagnostic"]
 
-    def __init__(self, name: str, icon: str, unit: str | None = None):
+    def __init__(
+        self, name: str, icon: str, unit: str | None = None, diagnostic: bool = False
+    ):
         self._name = name
         self._icon = icon
         self._unit = unit
+
+        self._is_diagnostic = diagnostic
 
     @property
     def name(self) -> str:
@@ -37,3 +42,7 @@ class BaseSubSensor:
             Set unit to "" for a "unitless" graph
         """
         return self._unit
+
+    @property
+    def diagnostic(self):
+        return self._is_diagnostic
