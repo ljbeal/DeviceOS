04e48ceb95d092f22a126f201b66dd14cb374ed3
ljbeal <louis.j.beal@gmail.com>
2024-06-17 17:26:10 +0200

    board is now available at toplevel

--- DeviceOS/__init__.py
+++ DeviceOS/__init__.py
@@ -1,7 +1,8 @@
+from DeviceOS.board.board import Board
 from DeviceOS.sensors.sensordevice import SensorDevice
 
 
-__all__ = ["SensorDevice"]
+__all__ = ["Board", "SensorDevice"]
 
 
 __version__ = "0.0.1a"
